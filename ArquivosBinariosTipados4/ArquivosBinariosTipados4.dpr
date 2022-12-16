7e2de013cba608f8ea5a5195e815c093d0053e00
L. Coelho <coelho.le@protonmail.com>
2022-12-15 23:47:02 -0300

    ;;
    ;;

--- ArquivosBinariosTipados4/ArquivosBinariosTipados4.dpr
+++ ArquivosBinariosTipados4/ArquivosBinariosTipados4.dpr
@@ -6,7 +6,7 @@ uses
   SysUtils;
 
 const
-  ARQUIVO_CIDADES='C:\Users\Administrador\Desktop\aulas-delphi\aulas-de-programacao-delphi\ArquivosBinariosTipados4\cidades';
+  ARQUIVO_CIDADES='C:\Users\leonardo\Desktop\aulas-delphi\aulas-de-programacao-delphi\ArquivosBinariosTipados4\cidades';
 
 type
   cidade=record
@@ -15,88 +15,169 @@ type
   end;
 
 var
-  op:string;
+  numreg:integer;//número de registros
 
-procedure imprimeMenu;
+procedure checkfile;
+var
+  cidadesdb:file of cidade;
 begin
-  writeln('1: CADASTRAR CIDADE');
-  writeln('2: LISTAR CIDADES');
-  writeln('3: MOSTRAR NUMERO DE CIDADES CADASTRADAS');
-  writeln('9: SAIR');
+  assignFile(cidadesdb,ARQUIVO_CIDADES);
+  if fileExists(ARQUIVO_CIDADES) then
+    reset(cidadesdb)
+  else
+    rewrite(cidadesdb);
+  numreg:=filesize(cidadesdb);
+  closeFile(cidadesdb);
+end;
+
+procedure imprimirMenu;
+begin
+  writeln;
+  writeln(#9,'1: CADASTRAR CIDADE');
+  writeln(#9,'2: LISTAR CIDADES');
+  writeln(#9,'3: MOSTRAR NUMERO DE CIDADES CADASTRADAS');
+  writeln(#9,'4: NAVEGAR PELOS REGISTROS');
+  writeln(#9,'9: SAIR');
   write('OP: ');
+end;
+
+function pedirUF:string;
+begin
+  repeat
+    write(#9,'Digite a UF: ');
+    readln(result);
+  until length(trim(result))=2;
+  result:=uppercase(result);
+end;
+
+function pedirCidade:string;
+begin
+  repeat
+    write(#9,'Digite o nome da cidade (3 ou mais caracteres): ');
+    readln(result);
+  until length(trim(result))>2;
+  result:=uppercase(result);
 end;
 
 procedure cadastrarCidade;
 var
-  nome,uf:string;
   cid:cidade;
   cidadesdb:file of cidade;
 begin
-  repeat
-    write('Digite a UF: ');
-    readln(uf);
-  until length(trim(uf))=2;
+  writeln;
 
-  repeat
-    write('Digite o nome da cidade (>= 3 caracteres): ');
-    readln(nome);
-  until length(trim(nome))>2;
-
-  cid.nome:=nome;
-  cid.uf:=uf;
+  cid.uf:=pedirUF;
+  cid.nome:=pedirCidade;
 
   assignFile(cidadesdb,ARQUIVO_CIDADES);
-  if fileExists(ARQUIVO_CIDADES) then begin
-    reset(cidadesdb);
-    seek(cidadesdb,filesize(cidadesdb));
-  end else
-    rewrite(cidadesdb);
+  reset(cidadesdb);
+  seek(cidadesdb,numreg);
   write(cidadesdb,cid);
   closeFile(cidadesdb);
+  inc(numreg);
 
-  imprimeMenu;
+  writeln;
+  imprimirMenu;
 end;
 
 procedure listarCidades;
 var
   cidadesdb:file of cidade;
   cid:cidade;
+  id:integer;
 begin
-  if not fileExists(ARQUIVO_CIDADES) then
-    exit;
-
+  writeln;
   assignFile(cidadesdb,ARQUIVO_CIDADES);
   reset(cidadesdb);
-
+  id:=0;
   while not eof(cidadesdb) do begin
     read(cidadesdb,cid);
-    writeln('NOME: ',cid.nome);
-    writeln('UF: ',cid.uf);
+    inc(id);
+    writeln(#9,'ID: ',id);
+    writeln(#9,'NOME: ',cid.nome);
+    writeln(#9,'UF: ',cid.uf);
     writeln;
   end;
   closeFile(cidadesdb);
 
-  imprimeMenu;
+  imprimirMenu;
 end;
 
 procedure mostrarNumeroCidades;
-var
-  cidadesdb:file of cidade;
 begin
-  //
-  //
-  //
+  writeln;
+  writeln(#9,'NUMERO DE CIDADES CADASTRADAS: ',numreg);
+  imprimirMenu;
+end;
+
+procedure navegarPelosRegistros;
+  var
+    op:string;//opção
+  procedure imprimirOpcoesDeNavegacao;
+  begin
+    writeln(#9#9,'a/A: IR PARA O PRIMEIRO REGISTRO');
+    writeln(#9#9,'b/B: IR PARA O REGISTRO ANTERIOR');
+    writeln(#9#9,'c/C: IR PARA O PROXIMO REGISTRO');
+    writeln(#9#9,'d/D: IR PARA O ULTIMO REGISTRO');
+    writeln(#9#9,'e/E: ESPECIFICAR O ID DO REGISTRO');
+    writeln(#9#9,'z/Z: VOLTAR PARA O MENU PRINCIPAL');
+    write(#9,'OP: ');
+  end;
+  procedure primeiroRegistro;
+  begin
+
+  end;
+  procedure registroAnterior;
+  begin
+
+  end;
+  procedure proximoRegistro;
+  begin
+
+  end;
+  procedure ultimoRegistro;
+  begin
+
+  end;
+  procedure especificarID;
+  begin
+
+  end;
+begin
+  op:='0';
+
+  repeat
+    if op[1]='A' then
+      primeiroRegistro
+    else
+    if op[1]='B' then
+      registroAnterior
+    else
+    if op[1]='C' then
+      proximoRegistro
+    else
+    if op[1]='D' then
+      ultimoRegistro
+    else
+    if op[1]='E' then
+      especificarID
+    else
+      imprimirOpcoesDeNavegacao;
+
+    readln(op);
+    op:=uppercase(op);
+
+    if length(op)<>1 then op:='0';
+  until op[1]='Z';
 end;
 
 procedure menu;
+var
+  op:string;//opção
 begin
-  imprimeMenu;
+  op:='0';
 
   repeat
-    readln(op);
-
-    if length(op)<>1 then op:='0';
-
     if op[1]='1' then
       cadastrarCidade
     else
@@ -104,10 +185,20 @@ begin
       listarCidades
     else
     if op[1]='3' then
-      mostrarNumeroCidades;
+      mostrarNumeroCidades
+    else
+    if op[1]='4' then
+      navegarPelosRegistros
+    else
+      imprimirMenu;
+
+    readln(op);
+
+    if length(op)<>1 then op:='0';
   until op[1]='9';
 end;
 
 begin
+  checkfile;
   menu;
 end.

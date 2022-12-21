848bf61ddd3049869c32e918901d7815e14848d7
Leonardo Coelho <l.coelho@sheths.com.br>
2022-12-21 09:53:07 -0300

    ;;
    ;;

--- ponteiros4/ponteiros4.dpr
+++ ponteiros4/ponteiros4.dpr
@@ -92,7 +92,7 @@ begin
   writeln('pint2^ = ',pint2^);//pint2^ coincide com pint1^ porque, agora, pint2 aponta para a mesma área de memória que pint1
   writeln;
 
-  int1:=2030;//pint1 aponta para int1, logo, a partir de agora, pint1^ = pint2^ = 2030
+  int1:=2030;//pint1 aponta para int1, logo, a partir de agora, pint1^ = pint2^ = int1 = 2030
   writeln('int1:=2030;');
   writeln('int1 = ',int1);
   writeln('Endereco de memoria guardado no ponteiro pint1: ',integer(pint1));
@@ -103,7 +103,7 @@ begin
   writeln('pint2^ = ',pint2^);
   writeln;
 
-  int2:=22;//pint1 não aponta para int2, bem como pint2 também não (porque fizemos pint2:=pint1), de modo que nada muda: ainda pint1^ = pint2^ = 2030 
+  int2:=22;//pint1 não aponta para int2, bem como pint2 também não (porque fizemos pint2:=pint1), de modo que nada muda: ainda pint1^ = pint2^ = int1 = 2030 
   writeln('int2:=22;');
   writeln('int2 = ',int2);
   writeln('Endereco de memoria guardado no ponteiro pint1: ',integer(pint1));
